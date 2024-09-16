2ce21a3b9687ecd62efd347285f8e6e7536e296e
Pablo Sebastián Caballero <pablocaballero1295@gmail.com>
2024-09-16 14:22:38 +0100

    Updated style to fit on mobile

---
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,91 @@
+import { useState, useRef, MouseEvent } from "react"
+
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import "./DropdownMenu.css"
+
+export const DropdownMenu = () => {
+  const menuButtonRef = useRef<HTMLDivElement>(null)
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  function scrollToElement(id: string) {
+    const element = document.getElementById(id)
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" })
+      handleClose()
+    }
+  }
+
+  return (
+    <div>
+      {/*
+    <Button
+      id="basic-button"
+      aria-controls={open ? 'basic-menu' : undefined}
+      aria-haspopup="true"
+      aria-expanded={open ? 'true' : undefined}
+      onClick={handleClick}
+    >
+      Dashboard
+    </Button>*/}
+      <i
+        ref={menuButtonRef}
+        className="fa-solid fa-bars"
+        onClick={handleClick}
+      ></i>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            scrollToElement("home")
+          }}
+        >
+          Home
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            scrollToElement("about")
+          }}
+        >
+          Sobre mí
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            scrollToElement("experience")
+          }}
+        >
+          Experiencia
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            scrollToElement("projects")
+          }}
+        >
+          Proyectos
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            scrollToElement("contact")
+          }}
+        >
+          Contacto
+        </MenuItem>
+      </Menu>
+    </div>
+  )
+}
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,23 @@
 import { NavBarButton } from "../NavBarButton/NavBarButton"
 import "./NavBar.css"
+import { DropdownMenu } from "../DropdownMenu/DropdownMenu"
 
 export const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar-content">
-        <div className="navbar-left-content">Pablo Sebastián Caballero</div>
+        <div className="navbar-left-content">Pablo Caballero</div>
+
         <div className="navbar-right-content">
           <NavBarButton text="Home" scrollTo="home" />
           <NavBarButton text="Sobre mí" scrollTo="about" />
           <NavBarButton text="Experiencia" scrollTo="experience" />
           <NavBarButton text="Proyectos" scrollTo="projects" />
           <NavBarButton text="Contacto" scrollTo="contact" />
+        </div>
+
+        <div className="navbar-right-content-mobile">
+          <DropdownMenu />
         </div>
       </div>
     </div>
